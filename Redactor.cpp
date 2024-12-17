5d3698e46dbd7ab587f6556f11734d4525c51680
Andrew <andrew.sc.porter@gmail.com>
2024-12-17 14:31:03 -0800

    class added

---
+++ Redactor.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Andrew on 12/17/2024.
+//
+
+#include "Redactor.hpp"
---
+++ Redactor.hpp
@@ -0,0 +1,14 @@
+//
+// Created by Andrew on 12/17/2024.
+//
+
+#ifndef REDAACTOR_REDACTOR_HPP
+#define REDAACTOR_REDACTOR_HPP
+
+
+class Redactor {
+
+};
+
+
+#endif //REDAACTOR_REDACTOR_HPP
